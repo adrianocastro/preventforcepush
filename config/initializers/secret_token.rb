3086b5fa25b2d6667f023ee1cb3d99a2b65cf0d2
Adriano Castro <ad@adrianocastro.net>
2015-05-27 15:21:25 -0700

    Created basic rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Preventforcepush::Application.config.secret_token = '960a9ebe67a6daff873f2489a173e89a838c080f698960a2162cf5af11f975bb564e6b8649b34a492760cc38fff3082dd07303abf02d20f160d08cce559f3eca'
